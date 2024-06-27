56edb0e071088eabd3d446576ad80daf7a9c74e3
Jan Nöhles <jan.noehles@easybill.de>
2024-06-27 14:14:58 +0200

    Add generators for enums Add generated enums
    Signed-off-by: Jan Nöhles <jan.noehles@easybill.de>

---
+++ src/Enums/CountryCodeEnum.php
@@ -0,0 +1,758 @@
+<?php
+
+enum CountryCode: string
+{
+    // ﻿Andorra
+    case AD = 'AD';
+
+    // United Arab Emirates (the)
+    case AE = 'AE';
+
+    // Afghanistan
+    case AF = 'AF';
+
+    // Antigua and Barbuda
+    case AG = 'AG';
+
+    // Anguilla
+    case AI = 'AI';
+
+    // Albania
+    case AL = 'AL';
+
+    // Armenia
+    case AM = 'AM';
+
+    // Angola
+    case AO = 'AO';
+
+    // Antarctica
+    case AQ = 'AQ';
+
+    // Argentina
+    case AR = 'AR';
+
+    // American Samoa
+    case AS = 'AS';
+
+    // Austria
+    case AT = 'AT';
+
+    // Australia
+    case AU = 'AU';
+
+    // Aruba
+    case AW = 'AW';
+
+    // Åland Islands
+    case AX = 'AX';
+
+    // Azerbaijan
+    case AZ = 'AZ';
+
+    // Bosnia and Herzegovina
+    case BA = 'BA';
+
+    // Barbados
+    case BB = 'BB';
+
+    // Bangladesh
+    case BD = 'BD';
+
+    // Belgium
+    case BE = 'BE';
+
+    // Burkina Faso
+    case BF = 'BF';
+
+    // Bulgaria
+    case BG = 'BG';
+
+    // Bahrain
+    case BH = 'BH';
+
+    // Burundi
+    case BI = 'BI';
+
+    // Benin
+    case BJ = 'BJ';
+
+    // Saint Barthélemy
+    case BL = 'BL';
+
+    // Bermuda
+    case BM = 'BM';
+
+    // Brunei Darussalam
+    case BN = 'BN';
+
+    // Bolivia (Plurinational State of)
+    case BO = 'BO';
+
+    // Bonaire, Sint Eustatius and Saba
+    case BQ = 'BQ';
+
+    // Brazil
+    case BR = 'BR';
+
+    // Bahamas (the)
+    case BS = 'BS';
+
+    // Bhutan
+    case BT = 'BT';
+
+    // Bouvet Island
+    case BV = 'BV';
+
+    // Botswana
+    case BW = 'BW';
+
+    // Belarus
+    case BY = 'BY';
+
+    // Belize
+    case BZ = 'BZ';
+
+    // Canada
+    case CA = 'CA';
+
+    // Cocos (Keeling) Islands (the)
+    case CC = 'CC';
+
+    // Congo (the Democratic Republic of the)
+    case CD = 'CD';
+
+    // Central African Republic (the)
+    case CF = 'CF';
+
+    // Congo (the)
+    case CG = 'CG';
+
+    // Switzerland
+    case CH = 'CH';
+
+    // Côte d'Ivoire
+    case CI = 'CI';
+
+    // Cook Islands (the)
+    case CK = 'CK';
+
+    // Chile
+    case CL = 'CL';
+
+    // Cameroon
+    case CM = 'CM';
+
+    // China
+    case CN = 'CN';
+
+    // Colombia
+    case CO = 'CO';
+
+    // Costa Rica
+    case CR = 'CR';
+
+    // Cuba
+    case CU = 'CU';
+
+    // Cabo Verde
+    case CV = 'CV';
+
+    // Curaçao
+    case CW = 'CW';
+
+    // Christmas Island
+    case CX = 'CX';
+
+    // Cyprus
+    case CY = 'CY';
+
+    // Czechia
+    case CZ = 'CZ';
+
+    // Germany
+    case DE = 'DE';
+
+    // Djibouti
+    case DJ = 'DJ';
+
+    // Denmark
+    case DK = 'DK';
+
+    // Dominica
+    case DM = 'DM';
+
+    // Dominican Republic (the)
+    case DO = 'DO';
+
+    // Algeria
+    case DZ = 'DZ';
+
+    // Ecuador
+    case EC = 'EC';
+
+    // Estonia
+    case EE = 'EE';
+
+    // Egypt
+    case EG = 'EG';
+
+    // Western Sahara*
+    case EH = 'EH';
+
+    // Eritrea
+    case ER = 'ER';
+
+    // Spain
+    case ES = 'ES';
+
+    // Ethiopia
+    case ET = 'ET';
+
+    // Finland
+    case FI = 'FI';
+
+    // Fiji
+    case FJ = 'FJ';
+
+    // Falkland Islands (the) [Malvinas]
+    case FK = 'FK';
+
+    // Micronesia (Federated States of)
+    case FM = 'FM';
+
+    // Faroe Islands (the)
+    case FO = 'FO';
+
+    // France
+    case FR = 'FR';
+
+    // Gabon
+    case GA = 'GA';
+
+    // United Kingdom of Great Britain and Northern Ireland (the)
+    case GB = 'GB';
+
+    // Grenada
+    case GD = 'GD';
+
+    // Georgia
+    case GE = 'GE';
+
+    // French Guiana
+    case GF = 'GF';
+
+    // Guernsey
+    case GG = 'GG';
+
+    // Ghana
+    case GH = 'GH';
+
+    // Gibraltar
+    case GI = 'GI';
+
+    // Greenland
+    case GL = 'GL';
+
+    // Gambia (the)
+    case GM = 'GM';
+
+    // Guinea
+    case GN = 'GN';
+
+    // Guadeloupe
+    case GP = 'GP';
+
+    // Equatorial Guinea
+    case GQ = 'GQ';
+
+    // Greece
+    case GR = 'GR';
+
+    // South Georgia and the South Sandwich Islands
+    case GS = 'GS';
+
+    // Guatemala
+    case GT = 'GT';
+
+    // Guam
+    case GU = 'GU';
+
+    // Guinea-Bissau
+    case GW = 'GW';
+
+    // Guyana
+    case GY = 'GY';
+
+    // Hong Kong
+    case HK = 'HK';
+
+    // Heard Island and McDonald Islands
+    case HM = 'HM';
+
+    // Honduras
+    case HN = 'HN';
+
+    // Croatia
+    case HR = 'HR';
+
+    // Haiti
+    case HT = 'HT';
+
+    // Hungary
+    case HU = 'HU';
+
+    // Indonesia
+    case ID = 'ID';
+
+    // Ireland
+    case IE = 'IE';
+
+    // Israel
+    case IL = 'IL';
+
+    // Isle of Man
+    case IM = 'IM';
+
+    // India
+    case IN = 'IN';
+
+    // British Indian Ocean Territory (the)
+    case IO = 'IO';
+
+    // Iraq
+    case IQ = 'IQ';
+
+    // Iran (Islamic Republic of)
+    case IR = 'IR';
+
+    // Iceland
+    case IS = 'IS';
+
+    // Italy
+    case IT = 'IT';
+
+    // Jersey
+    case JE = 'JE';
+
+    // Jamaica
+    case JM = 'JM';
+
+    // Jordan
+    case JO = 'JO';
+
+    // Japan
+    case JP = 'JP';
+
+    // Kenya
+    case KE = 'KE';
+
+    // Kyrgyzstan
+    case KG = 'KG';
+
+    // Cambodia
+    case KH = 'KH';
+
+    // Kiribati
+    case KI = 'KI';
+
+    // Comoros (the)
+    case KM = 'KM';
+
+    // Saint Kitts and Nevis
+    case KN = 'KN';
+
+    // Korea (the Democratic People's Republic of)
+    case KP = 'KP';
+
+    // Korea (the Republic of)
+    case KR = 'KR';
+
+    // Kuwait
+    case KW = 'KW';
+
+    // Cayman Islands (the)
+    case KY = 'KY';
+
+    // Kazakhstan
+    case KZ = 'KZ';
+
+    // Lao People's Democratic Republic (the)
+    case LA = 'LA';
+
+    // Lebanon
+    case LB = 'LB';
+
+    // Saint Lucia
+    case LC = 'LC';
+
+    // Liechtenstein
+    case LI = 'LI';
+
+    // Sri Lanka
+    case LK = 'LK';
+
+    // Liberia
+    case LR = 'LR';
+
+    // Lesotho
+    case LS = 'LS';
+
+    // Lithuania
+    case LT = 'LT';
+
+    // Luxembourg
+    case LU = 'LU';
+
+    // Latvia
+    case LV = 'LV';
+
+    // Libya
+    case LY = 'LY';
+
+    // Morocco
+    case MA = 'MA';
+
+    // Monaco
+    case MC = 'MC';
+
+    // Moldova (the Republic of)
+    case MD = 'MD';
+
+    // Montenegro
+    case ME = 'ME';
+
+    // Saint Martin (French part)
+    case MF = 'MF';
+
+    // Madagascar
+    case MG = 'MG';
+
+    // Marshall Islands (the)
+    case MH = 'MH';
+
+    // North Macedonia
+    case MK = 'MK';
+
+    // Mali
+    case ML = 'ML';
+
+    // Myanmar
+    case MM = 'MM';
+
+    // Mongolia
+    case MN = 'MN';
+
+    // Macao
+    case MO = 'MO';
+
+    // Northern Mariana Islands (the)
+    case MP = 'MP';
+
+    // Martinique
+    case MQ = 'MQ';
+
+    // Mauritania
+    case MR = 'MR';
+
+    // Montserrat
+    case MS = 'MS';
+
+    // Malta
+    case MT = 'MT';
+
+    // Mauritius
+    case MU = 'MU';
+
+    // Maldives
+    case MV = 'MV';
+
+    // Malawi
+    case MW = 'MW';
+
+    // Mexico
+    case MX = 'MX';
+
+    // Malaysia
+    case MY = 'MY';
+
+    // Mozambique
+    case MZ = 'MZ';
+
+    // Namibia
+    case NA = 'NA';
+
+    // New Caledonia
+    case NC = 'NC';
+
+    // Niger (the)
+    case NE = 'NE';
+
+    // Norfolk Island
+    case NF = 'NF';
+
+    // Nigeria
+    case NG = 'NG';
+
+    // Nicaragua
+    case NI = 'NI';
+
+    // Netherlands (the)
+    case NL = 'NL';
+
+    // Norway
+    case NO = 'NO';
+
+    // Nepal
+    case NP = 'NP';
+
+    // Nauru
+    case NR = 'NR';
+
+    // Niue
+    case NU = 'NU';
+
+    // New Zealand
+    case NZ = 'NZ';
+
+    // Oman
+    case OM = 'OM';
+
+    // Panama
+    case PA = 'PA';
+
+    // Peru
+    case PE = 'PE';
+
+    // French Polynesia
+    case PF = 'PF';
+
+    // Papua New Guinea
+    case PG = 'PG';
+
+    // Philippines (the)
+    case PH = 'PH';
+
+    // Pakistan
+    case PK = 'PK';
+
+    // Poland
+    case PL = 'PL';
+
+    // Saint Pierre and Miquelon
+    case PM = 'PM';
+
+    // Pitcairn
+    case PN = 'PN';
+
+    // Puerto Rico
+    case PR = 'PR';
+
+    // Palestine, State of
+    case PS = 'PS';
+
+    // Portugal
+    case PT = 'PT';
+
+    // Palau
+    case PW = 'PW';
+
+    // Paraguay
+    case PY = 'PY';
+
+    // Qatar
+    case QA = 'QA';
+
+    // Réunion
+    case RE = 'RE';
+
+    // Romania
+    case RO = 'RO';
+
+    // Serbia
+    case RS = 'RS';
+
+    // Russian Federation (the)
+    case RU = 'RU';
+
+    // Rwanda
+    case RW = 'RW';
+
+    // Saudi Arabia
+    case SA = 'SA';
+
+    // Solomon Islands
+    case SB = 'SB';
+
+    // Seychelles
+    case SC = 'SC';
+
+    // Sudan (the)
+    case SD = 'SD';
+
+    // Sweden
+    case SE = 'SE';
+
+    // Singapore
+    case SG = 'SG';
+
+    // Saint Helena, Ascension and Tristan da Cunha
+    case SH = 'SH';
+
+    // Slovenia
+    case SI = 'SI';
+
+    // Svalbard and Jan Mayen
+    case SJ = 'SJ';
+
+    // Slovakia
+    case SK = 'SK';
+
+    // Sierra Leone
+    case SL = 'SL';
+
+    // San Marino
+    case SM = 'SM';
+
+    // Senegal
+    case SN = 'SN';
+
+    // Somalia
+    case SO = 'SO';
+
+    // Suriname
+    case SR = 'SR';
+
+    // South Sudan
+    case SS = 'SS';
+
+    // Sao Tome and Principe
+    case ST = 'ST';
+
+    // El Salvador
+    case SV = 'SV';
+
+    // Sint Maarten (Dutch part)
+    case SX = 'SX';
+
+    // Syrian Arab Republic (the)
+    case SY = 'SY';
+
+    // Eswatini
+    case SZ = 'SZ';
+
+    // Turks and Caicos Islands (the)
+    case TC = 'TC';
+
+    // Chad
+    case TD = 'TD';
+
+    // French Southern Territories (the)
+    case TF = 'TF';
+
+    // Togo
+    case TG = 'TG';
+
+    // Thailand
+    case TH = 'TH';
+
+    // Tajikistan
+    case TJ = 'TJ';
+
+    // Tokelau
+    case TK = 'TK';
+
+    // Timor-Leste
+    case TL = 'TL';
+
+    // Turkmenistan
+    case TM = 'TM';
+
+    // Tunisia
+    case TN = 'TN';
+
+    // Tonga
+    case TO = 'TO';
+
+    // Turkey
+    case TR = 'TR';
+
+    // Trinidad and Tobago
+    case TT = 'TT';
+
+    // Tuvalu
+    case TV = 'TV';
+
+    // Taiwan (Province of China)
+    case TW = 'TW';
+
+    // Tanzania, the United Republic of
+    case TZ = 'TZ';
+
+    // Ukraine
+    case UA = 'UA';
+
+    // Uganda
+    case UG = 'UG';
+
+    // United States Minor Outlying Islands (the)
+    case UM = 'UM';
+
+    // United States of America (the)
+    case US = 'US';
+
+    // Uruguay
+    case UY = 'UY';
+
+    // Uzbekistan
+    case UZ = 'UZ';
+
+    // Holy See (the)
+    case VA = 'VA';
+
+    // Saint Vincent and the Grenadines
+    case VC = 'VC';
+
+    // Venezuela (Bolivarian Republic of)
+    case VE = 'VE';
+
+    // Virgin Islands (British)
+    case VG = 'VG';
+
+    // Virgin Islands (U.S.)
+    case VI = 'VI';
+
+    // Viet Nam
+    case VN = 'VN';
+
+    // Vanuatu
+    case VU = 'VU';
+
+    // Wallis and Futuna
+    case WF = 'WF';
+
+    // Samoa
+    case WS = 'WS';
+
+    // Yemen
+    case YE = 'YE';
+
+    // Mayotte
+    case YT = 'YT';
+
+    // South Africa
+    case ZA = 'ZA';
+
+    // Zambia
+    case ZM = 'ZM';
+
+    // Zimbabwe
+    case ZW = 'ZW';
+
+    // Kosovo
+    case _1A = '1A';
+
+    // United Kingdom (Northern Ireland)
+    case XI = 'XI';
+
+}
---
+++ tools/Generators/CountryCodeEnumGenerator.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+/** @return array<string, string> */
+function parseCSVFile(string $filename): array
+{
+    $countries = [];
+    if (($handle = fopen($filename, 'r')) !== false) {
+        while (($data = fgetcsv($handle, 1000, ';')) !== false) {
+            if (count($data) >= 2) {
+                $countries[trim($data[0])] = trim($data[1]);
+            }
+        }
+        fclose($handle);
+    }
+    return $countries;
+}
+
+/** @param array<string, string> $countries */
+function generateEnum(array $countries): string
+{
+    $enumCode = "enum CountryCode: string\n{\n";
+    foreach ($countries as $name => $code) {
+        $enumCode .= '    // ' . $name . "\n";
+        $enumCode .= '    case ' . $code . " = '" . $code . "';\n\n";
+    }
+    $enumCode .= "}\n";
+    return $enumCode;
+}
+
+$csvFilePath = __DIR__ . '/input/CountryCodes.csv';
+
+$countries = parseCSVFile($csvFilePath);
+
+$enumCode = generateEnum($countries);
+
+file_put_contents(__DIR__ . '/../../src/Enums/CountryCodeEnum.php', "<?php\n\n" . $enumCode);
