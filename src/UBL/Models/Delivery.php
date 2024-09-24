b8496f083a64d6e3a244a8f3274e0a008ae587a8
Jan Nöhles <jan.noehles@easybill.de>
2024-09-24 11:29:47 +0200

    Fix property name for ActualDeliveryDate in Delivery UBL
    Signed-off-by: Jan Nöhles <jan.noehles@easybill.de>

--- src/UBL/Models/Delivery.php
+++ src/UBL/Models/Delivery.php
@@ -13,7 +13,7 @@ final class Delivery
     #[Type('string')]
     #[XmlElement(cdata: false, namespace: 'urn:oasis:names:specification:ubl:schema:xsd:CommonBasicComponents-2')]
     #[SerializedName('ActualDeliveryDate')]
-    public ?string $endpointId = null;
+    public ?string $actualDeliveryDate = null;
 
     #[Type(DeliveryLocation::class)]
     #[XmlElement(cdata: false, namespace: 'urn:oasis:names:specification:ubl:schema:xsd:CommonAggregateComponents-2')]
